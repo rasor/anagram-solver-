4b3f5a4da5b43c64298864e3d43ea3b89fb88b68
rasor <soren@rasor.dk>
2018-02-10 14:58:03 +0100

    dotnet new console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace anagramsolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
