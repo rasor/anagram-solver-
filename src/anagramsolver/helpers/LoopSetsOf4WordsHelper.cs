6aedf0a701c9295b2fb4846b442ff4e392c8ad81
rasor <soren@rasor.dk>
2018-02-18 16:34:07 +0100

    CurrentSetOf4Pos.SetNextPos() done

---
+++ src/anagramsolver/helpers/LoopSetsOf4WordsHelper.cs
@@ -0,0 +1,132 @@
+﻿using anagramsolver.containers;
+using anagramsolver.models;
+using System;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace anagramsolver.helpers
+{
+    public class LoopSetsOf4WordsHelper: LoopSetsBase
+    {
+        public LoopSetsOf4WordsHelper(Action<string> ConsoleWriteLine, MD5 Md5HashComputer,
+            AnagramContainer AnagramCtrl, WordlistContainer WordlistCtrl) :
+            base(ConsoleWriteLine, Md5HashComputer, AnagramCtrl, WordlistCtrl)
+        { }
+
+        public int LoopSetsOf4WordsDoValidateAndCheckMd5(int numberOfJackpots)
+        {
+            UInt64 combinationCounter = 0; // max 18.446.744.073.709.551.615 .... yarn
+            UInt64 subsetCounter = 0; // count number of combinations that is also subset of anagram
+            // If the program does not check md5 if finds Combinations: 83.743.632 having Subsets: 5672 from the wordlist
+
+            var tableToLoopThrough = _wordlistCtrl.TableByWordLength;
+            var totalLetters = _anagramCtrl.Anagram.RawDataWithoutSpace.Length; //18
+            var hasUnEvenChars = totalLetters % 2; //if even the then the middle words are both first and last word - so that row in the table needs special looping
+            var middleWordLetters = (totalLetters + hasUnEvenChars) / 2;
+
+            // Set initial set - [1, 1, 1, 15] - a total of 18 chars
+            CurrentSetOf4Pos currentSetLength = new CurrentSetOf4Pos(totalLetters);
+            // Loop initial set - [1, 1, 1, 15]
+            //numberOfJackpots += Loop4WordCombinationsInCurrentSet(currentSetLength, ref combinationCounter, ref subsetCounter);
+            _consoleWriteLine(" Combinations: " + combinationCounter + ". Subsets: " + subsetCounter + ". CurrentSet: " + currentSetLength.ToString());
+            // Continue with the rest of the sets - downto set [9, 9]
+            while (currentSetLength.SetNextSet())
+            {
+                //numberOfJackpots += Loop4WordCombinationsInCurrentSet(currentSetLength, ref combinationCounter, ref subsetCounter);
+                _consoleWriteLine(" Combinations: " + combinationCounter + ". Subsets: " + subsetCounter + ". CurrentSet: " + currentSetLength.ToString());
+            }
+            _consoleWriteLine(" Combinations: " + combinationCounter + ". Subsets: " + subsetCounter + ". No more sets");
+
+            return numberOfJackpots;
+        }
+
+        private int Loop4WordCombinationsInCurrentSet(CurrentSetOf4Pos currentSetLength, ref ulong combinationCounter, ref ulong subsetCounter)
+        {
+            int numberOfJackpots = 0;
+            _consoleWriteLine(" Combinations: " + combinationCounter + ". Subsets: " + subsetCounter + ". CurrentSet: " + currentSetLength.ToString());
+
+            var listOfPointersToWord3 = _tableByWordLength[currentSetLength.Word3Length];
+            var listOfPointersToWord2 = _tableByWordLength[currentSetLength.Word2Length];
+            var listOfPointersToWord1 = _tableByWordLength[currentSetLength.Word1Length];
+
+            // Since we know that there won't be any long words before len = 11, then we make the outer loop pass those 0 values first
+            foreach (var word3Pointer in listOfPointersToWord3)
+            {
+                foreach (var word2Pointer in listOfPointersToWord2)
+                {
+                    foreach (var word1Pointer in listOfPointersToWord1)
+                    {
+                        // ConsoleWriteLine(" Combinations: " + combinationCounter + ". Subsets: " + subsetCounter);
+
+                        var word1Row = _wordlistCtrl.TableFilter2_WordMatrix[word1Pointer];
+                        var word2Row = _wordlistCtrl.TableFilter2_WordMatrix[word2Pointer];
+                        var word3Row = _wordlistCtrl.TableFilter2_WordMatrix[word3Pointer];
+                        var combinedWordToValidate = CombineRows(word1Row, word2Row, word3Row);
+                        var isSubset = _anagramCtrl.IsSubset(combinedWordToValidate);
+
+                        // Do MD5 check if the two words combined is still a subset of anagram
+                        bool gotJackpot = false;
+                        if (isSubset)
+                        {
+                            subsetCounter++;
+                            var word1 = _wordlistCtrl.ListFilter1_WorddictHavingAllowedChars.Keys.ElementAt(word1Pointer);
+                            var word2 = _wordlistCtrl.ListFilter1_WorddictHavingAllowedChars.Keys.ElementAt(word2Pointer);
+                            var word3 = _wordlistCtrl.ListFilter1_WorddictHavingAllowedChars.Keys.ElementAt(word3Pointer);
+
+                            gotJackpot = LoopPermutationsAndCheckMd5(ref numberOfJackpots, word1, word2, word3);
+                        }
+                        combinationCounter++;
+                    }
+                }
+            }
+            return numberOfJackpots;
+        }
+
+        /// <summary>
+        /// When this method is called we know that the characters in the sentence match (isSubset of) anagram
+        /// In here we loop through the order of the words and check md5
+        /// </summary>
+        /// <param name="numberOfJackpots"></param>
+        /// <param name="word1"></param>
+        /// <param name="word2"></param>
+        /// <param name="word3"></param>
+        /// <returns></returns>
+        private bool LoopPermutationsAndCheckMd5(ref int numberOfJackpots, string word1, string word2, string word3)
+        {
+            bool gotJackpot = false;
+            // did we get lucky?
+            if (!gotJackpot) { gotJackpot = checkMd5(ref numberOfJackpots, word1 + " " + word2 + " " + word3); }
+            if (!gotJackpot) { gotJackpot = checkMd5(ref numberOfJackpots, word1 + " " + word3 + " " + word2); }
+            if (!gotJackpot) { gotJackpot = checkMd5(ref numberOfJackpots, word2 + " " + word1 + " " + word3); }
+            if (!gotJackpot) { gotJackpot = checkMd5(ref numberOfJackpots, word2 + " " + word3 + " " + word1); }
+            if (!gotJackpot) { gotJackpot = checkMd5(ref numberOfJackpots, word3 + " " + word1 + " " + word2); }
+            if (!gotJackpot) { gotJackpot = checkMd5(ref numberOfJackpots, word3 + " " + word2 + " " + word1); }
+
+            return gotJackpot;
+        }
+
+        /// <summary>
+        /// Add number of each letter of three words, 
+        /// so the sum can be compared with the sum in the anagram
+        /// </summary>
+        /// <param name="row1">number of each letter in word1</param>
+        /// <param name="row2">number of each letter in word2</param>
+        /// <param name="row3">number of each letter in word3</param>
+        /// <returns>number of each letter in both words</returns>
+        private int[] CombineRows(int[] row1, int[] row2, int[] row3)
+        {
+            // Make a copy of row3
+            int[] combinedRow = (int[])row3.Clone();
+
+            // Word is stored from col3 onwards - loop it.
+            // Col1 is number of chars
+            for (int i = 1; i < row1.Length - 1; i++)
+            {
+                // Add row1 and row2 to row3
+                combinedRow[i] += (row1[i]+ row2[i]);
+            }
+
+            return combinedRow;
+        }
+    }
+}
--- src/anagramsolver/models/CurrentSetOf4Pos.cs
+++ src/anagramsolver/models/CurrentSetOf4Pos.cs
@@ -7,37 +7,46 @@ namespace anagramsolver.models
     /// <summary>
     /// Contains length of three words, which combined is length of anagram
     /// </summary>
-    public class CurrentSetOfThreePos: CurrentSetOfTwoPos
+    public class CurrentSetOf4Pos: CurrentSetOfThreePos
     {
-        protected int _word3Length;
-        public int Word3Length { get { return _word3Length - 1; } } //subtract 1 to match index in table
+        protected int _word4Length;
+        public int Word4Length { get { return _word4Length - 1; } } //subtract 1 to match index in table
 
-        public CurrentSetOfThreePos(int AnagramLength): 
+        public CurrentSetOf4Pos(int AnagramLength): 
             base(AnagramLength)
         {
             _word2Length = 1;
-            _word3Length = AnagramLength - 2;
+            _word3Length = 1;
+            _word4Length = AnagramLength - 3;
         }
 
         public override bool SetNextSet()
         {
             bool nextSetWasSet = true;
+            int diffWord4_3Len = _word4Length - _word3Length;
             int diffWord3_2Len = _word3Length - _word2Length;
             // if last cannot be decremented more, then increment other numbers
-            if (diffWord3_2Len == 0 || diffWord3_2Len == 1)
+            if ((diffWord4_3Len == 0 || diffWord4_3Len == 1) && (diffWord3_2Len == 0 || diffWord3_2Len == 1))
             {
                 _word1Length++;
                 _word2Length = _word1Length;
+                _word3Length = _word1Length;
+            }
+            else
+            if (diffWord4_3Len == 0 || diffWord4_3Len == 1)
+            {
+                _word2Length++;
+                _word3Length = _word2Length;
             }
             else
             {
-                _word2Length++;
+                _word3Length++;
             }
             // sum of all wordlenghts reamins 18
-            _word3Length = _anagramLength - (_word1Length + _word2Length);
+            _word4Length = _anagramLength - (_word1Length + _word2Length + _word3Length);
 
             // Stop if any left side numbers are larger than numbers to its rigth side
-            if (_word1Length > _word2Length || _word2Length > _word3Length)
+            if (_word1Length > _word2Length || _word2Length > _word3Length || _word3Length > _word4Length)
             {
                 nextSetWasSet = false;
             }
@@ -46,7 +55,7 @@ namespace anagramsolver.models
 
         public override string ToString()
         {
-            string result = "[" + _word1Length + ", " + _word2Length + ", " + _word3Length + "]";
+            string result = "[" + _word1Length + ", " + _word2Length + ", " + _word3Length + ", " + _word4Length + "]";
             return result;
         }
     }
