1f8b68d79de3c14c9fc1a67beb883b502ebe69ea
rasor <soren@rasor.dk>
2018-02-21 19:51:56 +0100

    currentSetCombinations changed to long

--- src/anagramsolver/helpers/LoopSetsOf5WordsHelper.cs
+++ src/anagramsolver/helpers/LoopSetsOf5WordsHelper.cs
@@ -47,7 +47,7 @@ namespace anagramsolver.helpers
             var listOfPointersToWord2 = _tableByWordLength[currentSetLength.Word2Length];
             var listOfPointersToWord1 = _tableByWordLength[currentSetLength.Word1Length];
 
-            var currentSetCombinations = listOfPointersToWord1.Count * listOfPointersToWord2.Count * listOfPointersToWord3.Count * listOfPointersToWord4.Count * listOfPointersToWord5.Count;
+            long currentSetCombinations = listOfPointersToWord1.Count * listOfPointersToWord2.Count * listOfPointersToWord3.Count * listOfPointersToWord4.Count * listOfPointersToWord5.Count;
             _consoleWriteLine(" Combinations: " + combinationCounter + ". Subsets: " + subsetCounter + ". NextSet: " + currentSetLength.ToString() + " having " + currentSetCombinations + " combinations");
 
             // List to avoid checking same sentence twice
