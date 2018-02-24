f2b4c4fc6896713098b86560d2e4d83e617ddf4e
rasor <soren@rasor.dk>
2018-02-24 20:24:57 +0100

    Moved the core logging to ConfigureLogging

--- src/anagramsolver/Program.cs
+++ src/anagramsolver/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using anagramsolver.containers;
-using anagramsolver.models;
-using System.Linq;
-using System.Security.Cryptography;
-using anagramsolver.helpers;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Debug;
 
 namespace anagramsolver
 {
@@ -21,20 +18,25 @@ namespace anagramsolver
             Startup startup = new Startup();
             startup.ConfigureServices(services);
             IServiceProvider serviceProvider = services.BuildServiceProvider();
-
-            ////configure console logging
-            //serviceProvider
-            //    .GetService<ILoggerFactory>()
-            //    .AddConsole(LogLevel.Debug);
-
-            //var logger = serviceProvider.GetService<ILoggerFactory>()
-            //    .CreateLogger<Program>();
-
-            //logger.LogDebug("Logger is working!");
+            //ConfigureLogging(serviceProvider);
 
             // Main() is moved to ProgramTransactionScript, so the Program.cs is only used for setting up DI
             var mainService = serviceProvider.GetService<ProgramTransactionScript>();
             mainService.Main(args);
+        }
+
+        private static void ConfigureLogging(IServiceProvider serviceProvider)
+        {
+            //configure console logging
+            serviceProvider
+                .GetService<ILoggerFactory>()
+                // Write to Console
+                .AddConsole(LogLevel.Information);
+            // Write to debugger
+            //.AddDebug(LogLevel.Trace);
+            var logger = serviceProvider.GetService<ILoggerFactory>()
+                .CreateLogger<Program>();
+            logger.LogError("Logging Error");
         }
     }
 }
